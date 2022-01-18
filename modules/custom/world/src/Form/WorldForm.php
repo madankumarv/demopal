74ed9cf58ead7616074283ec2899fd629c30dab4
Madan Kumar <36560562+madankumarv@users.noreply.github.com>
2022-01-18 10:46:46 +0530

    Update WorldForm.php

--- modules/custom/world/src/Form/WorldForm.php
+++ modules/custom/world/src/Form/WorldForm.php
@@ -39,7 +39,7 @@ class WorldForm extends FormBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $this->messenger()->addStatus($form_state->getValue('country'));
-    if (ctype_alpha($form_state->getValue('country'))) {
+    if (!ctype_alpha($form_state->getValue('country'))) {
       $form_state->setErrorByName('country', $this->t('Please enter only alphabets!'));
     }
   }
