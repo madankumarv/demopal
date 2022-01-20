bd15779f0bb16fca7d8210a5ccf2d298d983722b
Madan Kumar <36560562+madankumarv@users.noreply.github.com>
2022-01-20 18:28:57 +0530

    Update HelloBlock.php

--- modules/custom/world/src/Plugin/Block/HelloBlock.php
+++ modules/custom/world/src/Plugin/Block/HelloBlock.php
@@ -20,8 +20,19 @@ class HelloBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+    $config = $this->getConfiguration();
+
+    if (!empty($config['hello_block_name'])) {
+      $name = $config['hello_block_name'];
+    }
+    else {
+      $name = $this->t('to no one');
+    }
+
     return [
-      '#markup' => $this->t('Hello, World!'),
+      '#markup' => $this->t('Hello @name!', [
+        '@name' => $name,
+      ]),
     ];
   }
 
@@ -50,15 +61,15 @@ class HelloBlock extends BlockBase {
   public function blockSubmit($form, FormStateInterface $form_state) {
     parent::blockSubmit($form, $form_state);
     $values = $form_state->getValues();
-    $this->configuration['hello_block_name'] = $values['hello_block_name'];
+    $this->configuration['hello_block_name'] = $form_state->getValue(['myfieldset', 'hello_block_name']);
   }
 
 /**
    * {@inheritdoc}
    */
   public function blockValidate($form, FormStateInterface $form_state) {
-    if ($form_state->getValue('hello_block_name') === 'John') {
-      $form_state->setErrorByName('hello_block_name', $this->t('You can not say hello to John.'));
+    if ($form_state->getValue('hello_block_name') === 'Pakistan') {
+      $form_state->setErrorByName('hello_block_name', $this->t('You can not say hello to Pakistan.'));
     }
   }
 
